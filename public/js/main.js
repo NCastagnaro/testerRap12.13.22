acbf32114e9f86c0b500c2fc433823b3788f6679
Nick Castagnaro <nickshs123@gmail.com>
2022-12-13 12:19:41 -0500

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ document.querySelector('button').addEventListener('click', getRapperName);
  async function getRapperName(){
     const rapperName = document.querySelector('input').value
     try{
-        const response =  await fetch(`/api?rapperName=${rapperName}`)
+        const response =  await fetch(`https://testerrapapi.onrender.com/api/${rapperName}`)
         const data = await response.json()
 
         console.log(data)
