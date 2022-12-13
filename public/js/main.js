1aa4feafd0f6e78a5245ab9fa54435e6a0403e17
Nick Castagnaro <nickshs123@gmail.com>
2022-12-13 12:13:59 -0500

    making first commit

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+
+document.querySelector('button').addEventListener('click', getRapperName);
+
+
+ async function getRapperName(){
+    const rapperName = document.querySelector('input').value
+    try{
+        const response =  await fetch(`/api?rapperName=${rapperName}`)
+        const data = await response.json()
+
+        console.log(data)
+        document.querySelector('h3').innerText = data.birthName
+    }catch(error){
+    console.log(error)
+    }
+}
